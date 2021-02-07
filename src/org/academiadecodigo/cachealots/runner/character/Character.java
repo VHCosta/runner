8fcfa557815fdb2a844d34f77886ed02f6429001
vhcosta <vitorhugo.gx@gmail.com>
2021-02-07 17:41:36 -0100

    update Character and CharacterType

--- src/org/academiadecodigo/cachealots/runner/character/Character.java
+++ src/org/academiadecodigo/cachealots/runner/character/Character.java
@@ -2,19 +2,23 @@ package org.academiadecodigo.cachealots.runner.character;
 
 import org.academiadecodigo.cachealots.runner.blocks.BlockType;
 import org.academiadecodigo.cachealots.runner.grid.Position;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 public class Character {
 
     // class with the characteristics of the running blocks
 
     private boolean crashed;
+    private Picture sprite;
     private Position pos;
     private CharacterType characterType;
 
     public Character(CharacterType characterType){
-        this.pos = pos;
-        this.crashed = false;
+        pos = new Position();
+
         this.characterType = characterType;
+
+        this.sprite = new Picture(pos.getCol(), pos.getRow(), characterType.getSource());
     }
 
     public Position getPos() {
